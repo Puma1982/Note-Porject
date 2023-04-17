0f761f3490a6c1f9b5442e056c5ca62bd2661c89
VIP-HACKER <griss_hai2005@hotmail.com>
2023-04-17 19:54:19 +0200

    port 500
    15:27

---
+++ NoteProject/app.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+const port = 5000 || process.env.PORT;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+//STATIC FILES
+app.use(express.static('public'));
+
+//TEMPLATING ENGINE
+app.use(expressLayouts);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    res.render('index')
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+}
+)
