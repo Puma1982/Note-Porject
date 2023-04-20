90da4ec8791d8ab62b23e90406daa913c2eb61e9
VIP-HACKER <griss_hai2005@hotmail.com>
2023-04-20 18:07:20 +0200

    Update dashboard.js

--- NoteProject/server/routes/dashboard.js
+++ NoteProject/server/routes/dashboard.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
+<<<<<<< Updated upstream
 const { isLoggedIn } = require('../middleware/checkAuth');
 const dashboardController = require('../controllers/dashboardController');
+=======
+const dashboardController = require('../controllers/dashboardController');
+
+>>>>>>> Stashed changes
 
 /**
  * Dashboard Routes
