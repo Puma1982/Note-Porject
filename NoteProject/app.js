c1a3e2986c01f98fe4b5b9c3c9e495f0c068bc81
VIP-HACKER <griss_hai2005@hotmail.com>
2023-04-20 18:07:14 +0200

    error
    1:05:23

--- NoteProject/app.js
+++ NoteProject/app.js
@@ -18,8 +18,8 @@ app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
 //ROUTES
-
 app.use('/', require('./server/routes/index'));
+app.use('/', require('./server/routes/dashboard'));
 
 //Handle 404
 app.get('*', function(req, res) {
