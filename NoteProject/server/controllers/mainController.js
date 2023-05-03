817e412526762e0a927a7727666a665fc23648bd
VIP-HACKER <griss_hai2005@hotmail.com>
2023-05-03 07:51:02 +0200

    errors49:34

--- NoteProject/server/controllers/mainController.js
+++ NoteProject/server/controllers/mainController.js
@@ -1,10 +1,9 @@
 /**
- * GET /
- * Homepage 
-*/
+ * GET /HOMEPAGE
+ */
 exports.homepage = async (req, res) => {
   const locals = {
-    title: "NodeJs Notes",
+    title: "NoteNotes",
     description: "Free NodeJS Notes App.",
   }
   res.render('index', {
@@ -13,27 +12,26 @@ exports.homepage = async (req, res) => {
   });
 }
 /**
- * GET /
- * About 
-*/
+ * GET /ABOUT
+ */
 exports.about = async (req, res) => {
   const locals = {
-    title: "About - NodeJs Notes",
+    title: "About - NoteNotes",
     description: "Free NodeJS Notes App.",
   }
   res.render('about', locals);
 },
 exports.features = async (req, res) => {
   const locals = {
-    title: "Features - NodeJs Notes",
-    description: "Free NodeJS Notes App.",
+    title: "Features - NoteNotes",
+    description: "Free NoteNotes App.",
   }
   res.render('features', locals);
 },
 exports.time = async (req, res) => {
   const locals = {
-    title: "Time - NodeJs Notes",
-    description: "Free NodeJS Notes App.",
+    title: "Time - NoteNotes",
+    description: "Free NoteNotes App.",
   }
   res.render('time', locals);
 }
